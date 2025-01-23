d6be344fb9e3b9445e5b8133a0b14f16932d4dda
SofteClouds <mckenkea@shernet.sheridancollege.ca>
2025-01-23 12:00:46 -0500

    Removed the animations, and made a script that checks if mouse is over a pixie

--- Assets/Mechanical Drawing/Scripts/FollowPlayer.cs
+++ Assets/Mechanical Drawing/Scripts/FollowPlayer.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public bool followPlayer = false;
+    bool isOver;
+
+
+    SpriteRenderer sr;
+    public GameObject pixie;
+
     // Start is called before the first frame update
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector2 pixiePos = pixie.transform.position;
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(mousePos.x >= pixiePos.x - 1 && mousePos.x <= pixiePos.x +1 && mousePos.y >= pixiePos.y - 1 && mousePos.y <= pixiePos.y + 1)
+        {
+            isOver = true;
+        }
+        else
+        {
+            isOver = false;
+        }
+
+        Debug.Log("Mouse: " + mousePos + "Pixie: " + pixiePos + isOver);
         
+        if (isOver)
+        {
+            followPlayer = true;
+        }
     }
 }
