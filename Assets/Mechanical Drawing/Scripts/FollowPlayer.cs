b17e942d26702c5d5cd8b2bca293b2a28eddc029
SofteClouds <kaimck05@gmail.com>
2025-01-29 12:25:32 -0500

    Added the character sprite and changed the follow script so it follows the brief

--- Assets/Mechanical Drawing/Scripts/FollowPlayer.cs
+++ Assets/Mechanical Drawing/Scripts/FollowPlayer.cs
@@ -11,8 +11,8 @@ public class FollowPlayer : MonoBehaviour
     public float boundry = 1;       // boundry sourrounding pixies for the mouse to be in
     public GameObject pixie;        // pixie object
     public float speed = 1f;        // pixie follow speed
-    public Transform currentPos;    // starting position
-    public Transform playerPos;     // ending position
+
+    //public Transform playerPos;     // ending position (Not allowed)
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +24,7 @@ public class FollowPlayer : MonoBehaviour
     void Update()
     {
 
-        Vector2 pixiePos = pixie.transform.position;                                // Vector2 that stores the pixies current position
+        Vector2 pixiePos = pixie.transform.position;                             // Vector2 that stores the pixies current position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);     // Vector2 that stores the mouses current position in a world point
 
         // Checked if the mouse is within the bounds of a pixie
@@ -39,7 +39,7 @@ public class FollowPlayer : MonoBehaviour
         if (wasOver)
         {
             //lerp (smoothly transition between) the pixies current position, and the players position with speed
-            transform.position = Vector2.Lerp(currentPos.position, playerPos.position, speed * Time.deltaTime);
+            transform.position = Vector2.Lerp(transform.position, mousePos, speed * Time.deltaTime);
         }
     }
 }
